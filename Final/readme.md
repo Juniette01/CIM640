09eb6513984754c3e8c353da43d0d08242beff0a
Juniette01 <31319552+Juniette01@users.noreply.github.com>
2017-11-14 13:46:18 -0500

    Final Folder

---
+++ Final/readme.md
@@ -0,0 +1,2 @@
+3 Possible Ideas
+
---
+++ week11/bubble/empty-example/sketch.js
@@ -0,0 +1,106 @@
+//var bubble1;
+//var bubble2;
+
+var multiBubble = [];
+
+function setup(){
+  createCanvas(400,400);
+
+//bubble1 = new bubble(random(width), random(height), random(100));
+//bubble2 = new bubble(random(width), random(height), random(100));
+    
+    for(var i = 0; i < 100; i++){
+        var bubbleC = color(random(256),random(256),random(256));
+        multiBubble.push(new bubble(random(width), random(height), random(100), bubbleC));
+    }
+
+}
+
+function draw(){
+  background(255);
+//  bubble1.display();
+//  bubble2.display();
+//  move();
+//  console.log(checkPos());
+    
+//    if(checkPos() == true){
+//        bubbleX = 0;
+//    }
+    
+     for(var i = 0; i < multiBubble.length; i++){
+        multiBubble[i].display();
+         multiBubble[i].moveX();
+         multiBubble[i].moveY();
+         
+         
+        if(multiBubble[i].checkPosX() == true){
+            multiBubble[i].x = 0;
+        }
+         
+         if(multiBubble[i].checkPosY() == true){
+            multiBubble[i].y = 0;
+        }
+    }
+}
+
+function bubble(tempX, tempY, tempDiameter, c, tempDir){
+    this.x = tempX;
+    this.y = tempY;
+    this.diameter = tempDiameter; 
+    this.color = c;
+    this.dirX = tempDirX;
+    
+    this.display = function(){
+        fill(this.color);
+        ellipse(this.x, this.y, this.diameter, this.diameter);
+    }
+    
+    this.moveX = function(){
+        if(this.dirX == true){
+        this.x++;
+        }else{
+            this.x--;
+        }
+    }
+    
+    this.checkPosX = function(){
+        if(this.x > width){
+            return 0;
+        }else if(this.x < 0){
+            return i;
+        }else{
+            return -1;
+        }
+        
+    }
+    
+    this.moveY = function(){
+        this.y++;
+    }
+    
+    this.checkPosY = function(){
+        if(this.y > height){
+            return true;
+        }
+    }
+    
+    
+}
+
+
+
+function display(){
+  ellipse(bubbleX,bubbleY,bubbleSize,bubbleSize);
+}
+
+function move(){
+  bubbleX++;
+}
+
+function checkPos(){
+  if(bubbleX > width){
+    return true;
+  }else{
+    return false;
+  }
+}
